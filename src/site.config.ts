6e53bcaf9c13477b71362b441b0067ecce35ae70
faris <faris.ni@outlook.com>
2026-07-29 01:25:25 +0800

    style: refine homepage and footer

--- src/site.config.ts
+++ src/site.config.ts
@@ -13,7 +13,7 @@ export const siteConfig: SiteConfig = {
 	*/
 	title: "Dust In The Wind",
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "Chris Williams",
+	author: "Faris",
 	// Used as the default description meta property and webmanifest description
 	description: "An opinionated starter theme for Astro",
 	// HTML lang property, found in src/layouts/Base.astro L:18 & astro.config.ts L:48
