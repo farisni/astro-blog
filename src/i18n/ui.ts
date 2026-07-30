0c036b3876a45a9a9bf737b6015d1cec035cd698
faris <faris.ni@outlook.com>
2026-07-31 02:25:31 +0800

    chore: stylize Chinese site title

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -93,9 +93,9 @@ export const ui: Record<Language, Translation> = {
     toc: 'Оглавление',
   },
   'zh': {
-    title: '风里尘埃',
+    title: '風里尘埃',
     subtitle: '再现版式之美',
-    description: 'Retypeset是一款基于Astro框架的静态博客主题，中文名为风里尘埃。本主题以活版印字为设计灵感，通过建立全新的视觉规范，对所有页面进行重新编排，打造纸质书页般的阅读体验，再现版式之美。所见皆为细节，方寸尽显优雅。',
+    description: 'Retypeset是一款基于Astro框架的静态博客主题，中文名为風里尘埃。本主题以活版印字为设计灵感，通过建立全新的视觉规范，对所有页面进行重新编排，打造纸质书页般的阅读体验，再现版式之美。所见皆为细节，方寸尽显优雅。',
     posts: '文章',
     tags: '标签',
     about: '关于',
